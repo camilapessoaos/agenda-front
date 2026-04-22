1a7614e06afc8bb265ed8942b55be003e79becbd
camilapessoaos <camilapessoaos@hotmail.com>
2026-04-22 08:28:39 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://agenda-back-sb2t.onrender.com';
+const API_URL = 'https://agenda-back-sb2t.onrender.com/api/entries';
 
 const form = document.getElementById('entry-form');
 const entryId = document.getElementById('entry-id');
